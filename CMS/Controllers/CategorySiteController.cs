bda4d58ad468eac468bfc5746b8f2dfad2c82f3f
Đức Anh <anhduc329@gmail.com>
2017-02-16 17:11:19 +0700

    update categorysite

--- CMS/Controllers/CategorySiteController.cs
+++ CMS/Controllers/CategorySiteController.cs
@@ -11,7 +11,7 @@ using WebBackendPlus.Controllers;
 
 namespace CMS.Controllers
 {
-    public class CategorySiteController : Controller
+    public class CategorySiteController : BaseAuthedController
     {
 
         // GET: CategorySite
